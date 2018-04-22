4cb2eb7e1a8015a26926a39dbc160eb3bc4adf00
PatrickLaflamme <patrickj.laflamme@gmail.com>
2018-04-22 12:58:28 -0700

    update download_data.py to reflect api updates

--- Data/download_data.py
+++ Data/download_data.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-import json
-import sys
 import subprocess as sp
 
-from numerapi.numerapi import NumerAPI
+from numerapi import NumerAPI
 
-def main(login_file):
+def downloadNumeraiData():
     
     # load the login file
     with open(login_file) as f:
@@ -23,18 +21,12 @@ def main(login_file):
     # most API calls do not require logging in
     napi = NumerAPI(verbosity="info")
 
-    # log in
-    credentials = napi.login(email=content[0], password=content[1], mfa_enabled=True)
-    print(json.dumps(credentials, indent=2))
-
     # download current dataset
-    dl_succeeded = napi.download_current_dataset(dest_path=dataset_parent_folder,
-                                                 unzip=True)
-    print("download succeeded: " + str(dl_succeeded))
+    dl_succeeded = napi.download_current_dataset(dest_path=dataset_parent_folder,unzip=True)
     
     sp.call("mv " + dataset_parent_folder + "/*.zip ZipFiles/", shell=True)
     sp.call("rm " + dataset_parent_folder + "/*/example*")
     
 if __name__=='__main__':
 	
-	main(sys.argv[1])
+	downloadNumeraiData()
